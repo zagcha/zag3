75ac8356fa3391b1a0444fb8a65cb36deb1a071b
zagcha <64097681+zagcha@users.noreply.github.com>
2020-04-29 06:01:46 +0300

    Update and rename kaz.js to kaz1.js

--- kaz1.js
+++ kaz1.js
@@ -36,7 +36,6 @@ $(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-l
 
 $(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
 <style>
-.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px;}
 ul.nav.nav-tabs.fl li a {      color: #A1A1A1;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
 ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
 .fr.co {
